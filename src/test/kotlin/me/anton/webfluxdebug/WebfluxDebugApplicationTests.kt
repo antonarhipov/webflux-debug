94bc834efcc2f0d01b39ab4c28f8d5c0fd96d3c5
anton <ant.arhipov@gmail.com>
2023-05-16 19:28:50 +0300

    Initial commit

---
+++ src/test/kotlin/me/anton/webfluxdebug/WebfluxDebugApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.anton.webfluxdebug
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxDebugApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
